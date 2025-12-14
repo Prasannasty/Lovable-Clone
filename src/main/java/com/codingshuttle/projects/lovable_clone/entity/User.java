b8c1237a6453c479d4276e0618271ad824d4b6de
lifesigns-prasannasty <prasanna03072003@gmail.com>
2025-12-14 20:52:55 +0530

    implemented an Project Service part and MapStruct mapping and Repository part

--- src/main/java/com/codingshuttle/projects/lovable_clone/entity/User.java
+++ src/main/java/com/codingshuttle/projects/lovable_clone/entity/User.java
@@ -1,22 +1,33 @@
 package com.codingshuttle.projects.lovable_clone.entity;
 
-import jakarta.persistence.Entity;
-import lombok.AccessLevel;
-import lombok.Getter;
-import lombok.Setter;
+import jakarta.persistence.*;
+import lombok.*;
 import lombok.experimental.FieldDefaults;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
 
 import java.time.Instant;
 @Getter
 @Setter
 @FieldDefaults(level = AccessLevel.PRIVATE)
+@Entity
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "users")
 public class User {
+    @Id
+            @GeneratedValue(strategy = GenerationType.IDENTITY)
     Long id;
     String email;
     String passwordHash;
     String name;
     String avatarUrl;
+
+    @CreationTimestamp
     Instant createdAt;
+
     Instant deletedAt;
+    @UpdateTimestamp
     Instant updatedAt;
 }
